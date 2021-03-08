64e768d7c94bc1f99cdd1763388c919f15b37077
xusirui <xusirui@tianyancha.com>
2021-03-08 10:07:28 +0800

    update

--- my_api/bin/start.py
+++ my_api/bin/start.py
@@ -9,9 +9,10 @@ sys.path.append(os.path.abspath(__file__).split("my_api")[0])
 
 from my_api.config import setting
 from my_api.lib.interface import my_app
-from flup.server.fcgi import WSGIServer
+# from flup.server.fcgi import WSGIServer
 
 my_app.secret_key = setting.SECRET_KEY
 if __name__ == '__main__':
-    WSGIServer(my_app, bindAddress=(setting.SERVER_HOST, setting.SERVER_PORT)).run()
+    # WSGIServer(my_app, bindAddress=(setting.SERVER_HOST, setting.SERVER_PORT)).run()
+    my_app.run(host=setting.SERVER_HOST, port=setting.SERVER_PORT, debug=False)
 
